1a0ba89da07f5b439e0bdf6c2aa405da13bd03f8
jcolayab <olaya.julio79@gmail.com>
2025-08-22 11:00:00 -0500

    Delete build/default directory

--- build/default/CMakeFiles/HelloComputations.dir/Computation.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/HelloComputations.dir/Computation.cpp.obj: \
- C:/Users/jcola/Documents/projects/c++/HelloComputations/Computation.cpp \
- C:/Users/jcola/Documents/projects/c++/HelloComputations/Computation.hpp
--- build/default/CMakeFiles/HelloComputations.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for HelloComputations.
-# This may be replaced when dependencies are built.
